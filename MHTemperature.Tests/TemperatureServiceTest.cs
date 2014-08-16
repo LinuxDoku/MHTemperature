6a5c46ed77e31021060ffc08f0e23e6a1cf0c281
Martin Lantzsch <martin@linux-doku.de>
2014-08-16 15:36:09 +0200

    unit tests for temperature service

---
+++ MHTemperature.Tests/TemperatureServiceTest.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace MHTemperature.Tests
+{
+	[TestFixture]
+	public class TemperatureServiceTest
+	{
+		/// <summary>
+		/// NOTE: Assert that you are connect to the internet when running test.
+		/// </summary>
+		[Test]
+		public void GetCurrentTemperature()
+		{
+			var service = new TemperatureService();
+			var temperature = service.Current();
+
+			Assert.NotNull(temperature);
+			Assert.NotNull(temperature.Swimmer);
+			Assert.NotNull(temperature.NonSwimmer);
+			Assert.NotNull(temperature.KidSplash);
+			Assert.NotNull(temperature.DateTime);
+		}
+	}
+}
+
