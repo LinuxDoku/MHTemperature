22956c198c7a1a758fa882803b8f68545587e49e
Martin Lantzsch <martin@linux-doku.de>
2015-08-29 20:09:59 +0200

    updated installers to latest residata platform

--- MHTemperature.Service.Web/Installer.cs
+++ MHTemperature.Service.Web/Installer.cs
@@ -1,31 +1,9 @@
-﻿using System.Collections;
-using System.ComponentModel;
-using System.Diagnostics;
+﻿using System.ComponentModel;
+using Residata.Platform.Contract.Server.Worker;
 
 namespace MHTemperature.Service {
     [RunInstaller(true)]
-    public class Installer : System.Configuration.Install.Installer {
-        public override void Install(IDictionary stateSaver) {
-            var assembly = Context.Parameters["assemblypath"];
-            stateSaver.Add("MHTemperature.Service.Web.exe", assembly);
-            ExecuteMainAssebly(assembly, "install start");
-
-            base.Install(stateSaver);
-        }
-
-        public override void Uninstall(IDictionary savedState) {
-            ExecuteMainAssebly(Context.Parameters["assemblypath"], "uninstall");
-
-            base.Uninstall(savedState);
-        }
-
-        private void ExecuteMainAssebly(string executable, string args) {
-            var processStart = new ProcessStartInfo(executable, args);
-            processStart.WindowStyle = ProcessWindowStyle.Hidden;
-
-            using (var process = Process.Start(processStart)) {
-                process.WaitForExit();
-            }
-        }
+    public class Installer : Residata.Platform.Server.Worker.InstallerBase {
+        public Installer() : base(InstallerType.Service) {}
     }
 }
