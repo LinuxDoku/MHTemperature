fe9fc10936d2a63da73f3e3bfb9c68759e0009b7
Martin Lantzsch <martin@linux-doku.de>
2016-05-04 20:54:01 +0200

    update ConsoleHost to C# 6

--- MHTemperature.Console/Program.cs
+++ MHTemperature.Console/Program.cs
@@ -1,27 +1,24 @@
 ﻿using System;
 using System.Globalization;
 
-namespace MHTemperature.ConsoleHost
-{
-	public class Program
-	{
-		public static void Main(string[] args)
-		{
-			var service = new TemperatureService();
-			var temperature = service.Current();
+namespace MHTemperature.ConsoleHost {
+    public static class Program {
+        public static void Main(string[] args) {
+            var service = new TemperatureService();
+            var temperature = service.Current();
 
-			if(temperature != null) {
-				Console.WriteLine("Aktuelle Wasser-Temperatur im Freibad Marienhöhe:");
-				Console.WriteLine();
-				Console.WriteLine("Schwimmer-, Sprungbecken          {0:F2} °C", temperature.Swimmer);
-				Console.WriteLine("Nichtschwimmer-, Rutschenbecken   {0:F2} °C", temperature.NonSwimmer);
-				Console.WriteLine("Kinderbecken                      {0:F2} °C", temperature.KidSplash);
-				Console.WriteLine();
-				Console.WriteLine("Zuletzt aktualisiert {0}", temperature.DateTime.ToString(CultureInfo.GetCultureInfoByIetfLanguageTag("de")));
-			    Console.ReadLine();
-			} else {
-				Console.WriteLine("Huch, die aktuelle Wassertemperatur konnte leider nicht abgerufen werden.");
-			}
-		}
-	}
+            if (temperature != null) {
+                Console.WriteLine("Aktuelle Wasser-Temperatur im Freibad Marienhöhe:");
+                Console.WriteLine();
+                Console.WriteLine($"Schwimmer-, Sprungbecken          {temperature.Swimmer:F2} °C");
+                Console.WriteLine($"Nichtschwimmer-, Rutschenbecken   {temperature.NonSwimmer:F2} °C");
+                Console.WriteLine($"Kinderbecken                      {temperature.KidSplash:F2} °C");
+                Console.WriteLine();
+                Console.WriteLine($"Zuletzt aktualisiert {temperature.DateTime.ToString(CultureInfo.GetCultureInfoByIetfLanguageTag("de"))}");
+                Console.ReadLine();
+            } else {
+                Console.WriteLine("Huch, die aktuelle Wassertemperatur konnte leider nicht abgerufen werden.");
+            }
+        }
+    }
 }
