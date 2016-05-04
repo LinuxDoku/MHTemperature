f6af5c35af983ccb352f588cea327f5cbe71a35c
Martin Lantzsch <martin@linux-doku.de>
2016-05-04 22:44:27 +0200

    add Schema.sql

---
+++ MHTemperature.Service/Data/Sql/Schema.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE public.temperatures
+(
+  id integer NOT NULL DEFAULT nextval('temperature_id_seq'::regclass),
+  swimmer numeric(4,2),
+  non_swimmer numeric(4,2),
+  kids_splash numeric(4,2),
+  measured_at timestamp without time zone,
+  CONSTRAINT pk PRIMARY KEY (id)
+)
+WITH (
+  OIDS=FALSE
+);
